e7ff007f26aafffce57b8a3ae2b878e66c163097
Mayco <mayco.s.berghetti@gmail.com>
2020-12-03 13:42:39 -0400

    module pclxl now work to count page

---
+++ build/lib/pkpgpdls/pil.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+#
+# pkpgcounter: a generic Page Description Language parser
+#
+# (c) 2003-2009 Jerome Alet <alet@librelogiciel.com>
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# $Id$
+#
+
+"""This modules implements a page counter for image formats supported by the Python Imaging Library."""
+
+from . import pdlparser
+
+try:
+    from PIL import Image
+except ImportError:
+    sys.stderr.write("ERROR: You MUST install the Python Imaging Library (python-imaging) for pkpgcounter to work.\n")
+    raise pdlparser.PDLParserError("The Python Imaging Library is missing.")
+
+from . import version
+
+class Parser(pdlparser.PDLParser):
+    """A parser for plain text documents."""
+    totiffcommands = [ 'convert "%(infname)s" "%(outfname)s"' ]
+    required = [ "convert" ]
+    def isValid(self):
+        """Returns True if data is an image format supported by PIL, else False."""
+        try:
+            image = Image.open(self.filename)
+        except (IOError, OverflowError):
+            return False
+        else:
+            self.format = "%s (%s)" % (image.format, image.format_description)
+            return True
+
+    def getJobSize(self):
+        """Counts pages in an image file."""
+        index = 0
+        image = Image.open(self.filename)
+        try:
+            while True:
+                index += 1
+                image.seek(index)
+        except EOFError:
+            pass
+        return index
---
+++ build/lib/pkpgpdls/version.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+#
+# pkpgcounter: a generic Page Description Language parser
+#
+# (c) 2003-2009 Jerome Alet <alet@librelogiciel.com>
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# $Id$
+#
+
+"""This modules defines some important constants used in this software."""
+
+import time
+
+__version__ = "3.51alpha"
+
+__doc__ = """pkpgcounter: a generic Page Description Languages parser."""
+
+__author__ = "Jerome Alet"
+__authoremail__ = "alet@librelogiciel.com"
+
+__years__ = "2003-%s" % time.strftime("%Y", time.localtime(time.time()))
+
+__gplblurb__ = """This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>."""
+
